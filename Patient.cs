96ec08906ff75777734ccc44a884d700c5f8cf6c
Simrandeep Kaur <136860085+simjhajj@users.noreply.github.com>
2024-02-19 01:25:12 -0500

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_C
+{
+    internal class Patient
+    {
+        // Properties with getters and setters
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+
+        // constructor for patient class
+        public Patient(string firstName, string lastName, double weight, double height)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Weight = weight;
+            Height = height;
+        }
+
+        // method to calculate Blood Pressure of a person
+        public string BloodPressure(int systolic, int diastolic)
+        {
+            if (systolic <= 120 || diastolic <= 80)
+            {
+                return "NORMAL";
+            }
+            else if (systolic <= 129 || diastolic <= 80)
+            {
+                return "ELEVATED";
+            }
+            else if (systolic <= 139 || diastolic <= 89)
+            {
+                return "HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 1";
+            }
+            else if (systolic >= 140 || diastolic >= 90)
+            {
+                return "HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 2";
+            }
+            else if (systolic >= 180 || diastolic >= 120)
+            {
+                return "HYPERTENSIVE CRISIS (consult your doctor immediately)";
+            }
+            else
+            {
+                return "Consult your doctor immediately";
+            }
+
+        }
+
+        // Method to calculate BMI of a person
+        public double CalculateBMI()
+        {
+
+            // conversion of height from cm to m
+            double heightInMetres = Height / 100.0;
+
+            // calculation of bmi
+            double bmi = Weight / (heightInMetres * heightInMetres);
+
+            return bmi;
+
+        }
+
+        // method to return patient information
+        public void PatientInfo(int systolic, int diastolic)
+        {
+            // calaculate blood pressure results
+            string bloodPressureResults = BloodPressure(systolic, diastolic);
+
+            // calculate BMI results
+            double bmiResults = CalculateBMI();
+
+            // print Patient's status based on BMI results
+            string bmiStatus = GetBmiStatus(bmiResults);
+
+            /// printing patient's information information
+            Console.WriteLine($"Full Name: {FirstName} {LastName}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"Blood Pressure Result: {bloodPressureResults}");
+            Console.WriteLine($"BMI Results: {bmiResults}");
+            Console.WriteLine($"BMI Status: {bmiStatus}");
+        }
+
+        // method to return BMI results based on different values
+        private static string GetBmiStatus(double bmi)
+        {
+
+            if (bmi >= 25.0)
+            {
+                return "Overweight";
+            }
+            else if (bmi >= 18.5 && bmi <= 24.9)
+            {
+                return "Normal (in the healthy range)";
+            }
+            else
+            {
+                return "Underweight";
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Assignment1_C;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // getting Patient's First Name
+            Console.WriteLine("Enter your First Name: ");
+            string firstName = Console.ReadLine();
+
+            // getting Patient's Last Name
+            Console.WriteLine("Enter your Last Name: ");
+            string lastName = Console.ReadLine();
+
+            // getting Patient's Weight in KG
+            Console.WriteLine("Enter your Weight in kg: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            // getting Patient's Height
+            Console.WriteLine("Enter your Height in metres: ");
+            double height = double.Parse(Console.ReadLine());
+
+            // getting Patient's Systolic Blood Pressure
+            Console.WriteLine("Enter your Systolic Blood Pressure: ");
+            int systolic = int.Parse(Console.ReadLine());
+
+            // getting Patient's Diastolic Blood Pressure
+            Console.WriteLine("Enter your Diastolic Blood Pressure: ");
+            int diastolic = int.Parse(Console.ReadLine());
+
+            // creating a new instance of Patient class
+            Patient patient = new Patient(firstName, lastName, weight, height);
+          
+            // printing patient's information using the object created
+            patient.PatientInfo(systolic, diastolic);
+        }
+    }
+}
